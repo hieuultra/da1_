c5a4efeff31baafb5ca6e0d95da5cc84239ecf17
hieuultra <147162531+hieuultra@users.noreply.github.com>
2023-11-18 23:02:26 +0700

    Update _category.php

--- user/home/_category.php
+++ user/home/_category.php
@@ -7,7 +7,6 @@
 <!-- Categories Start -->
 <div class="container-fluid pt-5">
   <div class="row px-xl-5 pb-3">
-
     <?php
     foreach ($dsdm as $ds) {
       extract($ds);
@@ -15,16 +14,29 @@
       $hinh = $img_path . $img_cat;
       $countsp = loadall_pro_count($ds['id_cat']);
       // $linkdm = "index.php?act=sanpham&id_dm=" . $id_dm;
-      echo ' 
-        <div class="col-lg-4 col-md-6 pb-1">
-          <div class="cat-item d-flex flex-column border mb-4" style="padding: 30px">
-            <p class="text-right">' . $countsp . ' products</p>
-            <a href="" class="cat-img position-relative overflow-hidden mb-3">
-            <a href="' . $linkdm . '">  <img class="img-fluid" src="' . $hinh . '" alt="" id="img"/></a>
-            </a>
-            <h5 class="font-weight-semi-bold m-0">' . $name_cat . '</h5>
-          </div>
-        </div>';
+      // echo ' 
+      //   <div class="col-lg-4 col-md-6 pb-1">
+      //     <div class="cat-item d-flex flex-column border mb-4" style="padding: 30px">
+      //       <p class="text-right">' . $countsp . ' products</p>
+      //       <a href="" class="cat-img position-relative overflow-hidden mb-3">
+      //       <a href="' . $linkdm . '">  <img class="img-fluid" src="' . $hinh . '" alt="" id="img"/></a>
+      //       </a>
+      //       <h5 class="font-weight-semi-bold m-0">' . $name_cat . '</h5>
+      //     </div>
+      //   </div>';
+      echo '   <div class="col-lg-2 col-md-6 pb-1">
+        <a class="text-decoration-none" href="">
+        <div class="cat-item d-flex align-items-center mb-4">
+            <div class="overflow-hidden" style="width: 100px; height: 100px;">
+            <a href="' . $linkdm . '">  <img class="img-fluid" src="' . $hinh . '" alt=""></a>
+            </div>
+            <div class="flex-fill pl-3">
+                <h6>' . $name_cat . '</h6>
+                <small class="text-body">' . $countsp . ' Products</small>
+            </div>
+        </div>
+    </a>
+    </div>';
     }
     ?>
 
