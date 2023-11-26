136e80c13ff225d1f5dfc364ffcda49878720895
hieuultra <147162531+hieuultra@users.noreply.github.com>
2023-11-26 23:47:17 +0700

    sl

--- user/home/_slideShow.php
+++ user/home/_slideShow.php
@@ -10,7 +10,7 @@
                 echo '';
               } ?>
              <div class="carousel-item active" style="height: 410px">
-               <img class="img-fluid" src="user/img/b5.jpg" alt="Image" />
+               <img class="img-fluid" src="user/img/s1.jpg" alt="Image" />
                <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                  <div class="p-3" style="max-width: 700px">
                    <h4 class="text-light text-uppercase font-weight-medium mb-3">
@@ -24,7 +24,7 @@
                </div>
              </div>
              <div class="carousel-item" style="height: 410px">
-               <img class="img-fluid" src="user/img/b2.jpg" alt="Image" />
+               <img class="img-fluid" src="user/img/s2.jpg" alt="Image" />
                <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                  <div class="p-3" style="max-width: 700px">
                    <h4 class="text-light text-uppercase font-weight-medium mb-3">
@@ -38,7 +38,7 @@
                </div>
              </div>
              <div class="carousel-item" style="height: 410px">
-               <img class="img-fluid" src="user/img/hatb.jpg" alt="Image" />
+               <img class="img-fluid" src="user/img/s3.jpg" alt="Image" />
                <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                  <div class="p-3" style="max-width: 700px">
                    <h4 class="text-light text-uppercase font-weight-medium mb-3">
