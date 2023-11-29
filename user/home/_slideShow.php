e0a44dd6f37364b97a892f1d54c318bfbc05c6e0
hieuultra <147162531+hieuultra@users.noreply.github.com>
2023-11-30 00:29:37 +0700

    sls

--- user/home/_slideShow.php
+++ user/home/_slideShow.php
@@ -1,69 +1,72 @@
-   <!-- Slide show -->
-   <div class="container-fluid mb-5">
-     <div class="row px-xl-5">
-       <div class="col-lg">
-         <div id="header-carousel" class="carousel slide" data-ride="carousel">
-           <div class="carousel-inner">
-             <?php foreach ($dssl as $ds) {
-                extract($ds);
-                $hinh = $img_path . $img_slider;
-                echo '';
-              } ?>
-             <div class="carousel-item active" style="height: 410px">
-               <img class="img-fluid" src="user/img/s1.jpg" alt="Image" />
-               <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
-                 <div class="p-3" style="max-width: 700px">
-                   <h4 class="text-light text-uppercase font-weight-medium mb-3">
-                     10% Off Your First Order
-                   </h4>
-                   <h3 class="display-4 text-white font-weight-semi-bold mb-4">
-                     Fashionable Dress
-                   </h3>
-                   <a href="" class="btn btn-light py-2 px-3">Shop Now</a>
-                 </div>
-               </div>
-             </div>
-             <div class="carousel-item" style="height: 410px">
-               <img class="img-fluid" src="user/img/s2.jpg" alt="Image" />
-               <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
-                 <div class="p-3" style="max-width: 700px">
-                   <h4 class="text-light text-uppercase font-weight-medium mb-3">
-                     10% Off Your First Order
-                   </h4>
-                   <h3 class="display-4 text-white font-weight-semi-bold mb-4">
-                     Reasonable Price
-                   </h3>
-                   <a href="" class="btn btn-light py-2 px-3">Shop Now</a>
-                 </div>
-               </div>
-             </div>
-             <div class="carousel-item" style="height: 410px">
-               <img class="img-fluid" src="user/img/s3.jpg" alt="Image" />
-               <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
-                 <div class="p-3" style="max-width: 700px">
-                   <h4 class="text-light text-uppercase font-weight-medium mb-3">
-                     10% Off Your First Order
-                   </h4>
-                   <h3 class="display-4 text-white font-weight-semi-bold mb-4">
-                     Reasonable Price
-                   </h3>
-                   <a href="" class="btn btn-light py-2 px-3">Shop Now</a>
-                 </div>
-               </div>
-             </div>
-           </div>
-           <a class="carousel-control-prev" href="#header-carousel" data-slide="prev">
-             <div class="btn btn-dark" style="width: 45px; height: 45px">
-               <span class="carousel-control-prev-icon mb-n2"></span>
-             </div>
-           </a>
-           <a class="carousel-control-next" href="#header-carousel" data-slide="next">
-             <div class="btn btn-dark" style="width: 45px; height: 45px">
-               <span class="carousel-control-next-icon mb-n2"></span>
-             </div>
-           </a>
-         </div>
-       </div>
-     </div>
-   </div>
-   <!-- Slide End -->
+<div class="container-fluid mb-3">
+  <div class="row px-xl-5">
+    <div class="col-lg-8">
+      <div id="header-carousel" class="carousel slide carousel-fade mb-30 mb-lg-0" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#header-carousel" data-slide-to="0" class="active"></li>
+          <li data-target="#header-carousel" data-slide-to="1"></li>
+          <li data-target="#header-carousel" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item position-relative active" style="height: 430px;">
+            <img class="position-absolute w-100 h-100" src="user/img/s1.jpg" style="object-fit: cover;">
+            <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
+              <div class="p-3" style="max-width: 700px;">
+                <h1 class="display-4 text-white mb-3 animate__animated animate__fadeInDown">Men Fashion</h1>
+                <p class="mx-md-5 px-5 animate__animated animate__bounceIn">Lorem rebum magna amet lorem magna erat diam stet. Sadips duo stet amet amet ndiam elitr ipsum diam</p>
+                <a class="btn btn-outline-light py-2 px-4 mt-3 animate__animated animate__fadeInUp" href="?act=shop1">Shop Now</a>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item position-relative" style="height: 430px;">
+            <img class="position-absolute w-100 h-100" src="user/img/s2.jpg" style="object-fit: cover;">
+            <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
+              <div class="p-3" style="max-width: 700px;">
+                <h1 class="display-4 text-white mb-3 animate__animated animate__fadeInDown">Women Fashion</h1>
+                <p class="mx-md-5 px-5 animate__animated animate__bounceIn">Lorem rebum magna amet lorem magna erat diam stet. Sadips duo stet amet amet ndiam elitr ipsum diam</p>
+                <a class="btn btn-outline-light py-2 px-4 mt-3 animate__animated animate__fadeInUp" href="?act=shop2">Shop Now</a>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item position-relative" style="height: 430px;">
+            <img class="position-absolute w-100 h-100" src="user/img/s3.jpg" style="object-fit: cover;">
+            <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
+              <div class="p-3" style="max-width: 700px;">
+                <h1 class="display-4 text-white mb-3 animate__animated animate__fadeInDown">Kids Fashion</h1>
+                <p class="mx-md-5 px-5 animate__animated animate__bounceIn">Lorem rebum magna amet lorem magna erat diam stet. Sadips duo stet amet amet ndiam elitr ipsum diam</p>
+                <a class="btn btn-outline-light py-2 px-4 mt-3 animate__animated animate__fadeInUp" href="#">Shop Now</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-lg-4">
+      <div class="row">
+        <div class="col-md-12 mb-4">
+          <div class="product-offer mb-2">
+            <img class="img-fluid w-100" src="user/img/b1.jpg" alt="">
+            <div class="offer-text position-absolute text-center w-100">
+              <h6 class="text-white text-uppercase">Save 20%</h6>
+              <h3 class="text-white mb-3">Special Offer</h3>
+              <a href="#" class="btn btn-primary">Shop Now</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-12">
+          <div class="product-offer mt-2">
+            <img class="img-fluid w-100" src="user/img/b_giay.jpg" alt="">
+            <div class="offer-text position-absolute text-center w-100">
+              <h6 class="text-white text-uppercase">Save 20%</h6>
+              <h3 class="text-white mb-3">Special Offer</h3>
+              <!-- <a href="#" class="btn btn-primary">Shop Now</a> -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+  </div>
+</div>
