661cea37b00a9e6f155f3fabadd47adce2eb588e
Trandat04 <149884022+Trandat04@users.noreply.github.com>
2023-11-30 22:28:09 +0700

    suamybill

--- user/mybill.php
+++ user/mybill.php
@@ -25,29 +25,34 @@
                             <th>Action</th>
                         </tr>
                     </thead>
-                
-                        <?php
-                        if (is_array($listbill)) {
-                            foreach ($listbill as $bill) {
-                                extract($bill);
-                                // $ttdh = get_ttdh($bill['id_status_bill']);
-                                $countsp = loadall_cart_count($id_bill);
-                                $ctb = "index.php?act=bill_detail&id_bill=" . $id_bill;
-                                $huydh = "index.php?act=update_bill&id_bill=" . $id_bill;
-                                echo ' <tr>
+
+                    <?php
+                    if (is_array($listbill)) {
+                        foreach ($listbill as $bill) {
+                            extract($bill);
+                            // $ttdh = get_ttdh($bill['id_status_bill']);
+                            $countsp = loadall_cart_count($id_bill);
+                            $ctb = "index.php?act=bill_detail&id_bill=" . $id_bill;
+                            $huydh = "index.php?act=update_bill&id_bill=" . $id_bill;
+                            if ($id_status_bill != 1) {
+                                $huydh = ''; // Không hiển thị nút xóa cho admin
+                            } else {
+                                $huydh = '<a href="' . $huydh . '" class="btn btn-danger"><input type="button" value="Unset bill" onclick ="return confirm(\'ban co chac chan muon huy don hang?\')" /></a>';
+                            }
+                            echo ' <tr>
                             <td>' . $id_bill . '</td>
                             <td>' . $date_order . '</td>
                             <td>' . $countsp . '</td>
                             <td>' . number_format($total_price, 0, ",", ".") . '$' . '</td>
                             <td>' . $name_status . '</td>
                             <td><a href="' . $ctb . '" class="btn btn-primary"><input type="button" value="See detail" /></a></td>
-                            <td> <a href="' . $huydh . '" class="btn btn-danger"><input type="button" value="Unset bill" onclick ="return confirm(\'ban co chac chan muon huy don hang?\')" /></a>
+                            <td> ' . $huydh . '
                           </td>
                                      </tr>';
-                            }
                         }
-                        ?>
-              
+                    }
+                    ?>
+
                 </table>
             </div>
         </div>
