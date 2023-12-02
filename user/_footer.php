f87660fce51141a9ab4041ce792adf2d0f2a82f9
hieuultra <147162531+hieuultra@users.noreply.github.com>
2023-12-02 07:40:19 +0700

    Update _footer.php

--- user/_footer.php
+++ user/_footer.php
@@ -1,10 +1,10 @@
 <!-- Footer Start -->
-<div class="container-fluid shadow-lg bg-light text-dark mt-5 pt-5">
+<div class="container-fluid shadow-lg bg-dark text-light mt-5 pt-5">
   <div class="row px-xl-5 pt-5">
     <div class="col-lg-4 col-md-12 mb-5 pr-3 pr-xl-5">
       <a href="" class="text-decoration-none">
         <h1 class="mb-4 display-5 font-weight-semi-bold">
-          <span class="text-primary font-weight-bold border border-white px-3 mr-1">U</span>Multishop
+          <span class="text-primary font-weight-bold border border-white px-3 mr-1">U Multishop</span>
         </h1>
       </a>
       <p>
@@ -24,34 +24,33 @@
     </div>
     <div class="col-lg-8 col-md-12">
       <div class="row">
+        <!-- Quick Links -->
         <div class="col-md-4 mb-5">
-          <h5 class="font-weight-bold text-dark mb-4">Quick Links</h5>
+          <h5 class="font-weight-bold text-light mb-4">Quick Links</h5>
           <div class="d-flex flex-column justify-content-start">
-            <a class="text-dark mb-2" href="index.html"><i class="fa fa-angle-right mr-2"></i>Home</a>
-            <a class="text-dark mb-2" href="shop.html"><i class="fa fa-angle-right mr-2"></i>Our Shop</a>
-            <a class="text-dark mb-2" href="detail.html"><i class="fa fa-angle-right mr-2"></i>Shop Detail</a>
-            <a class="text-dark mb-2" href="cart.html"><i class="fa fa-angle-right mr-2"></i>Shopping Cart</a>
-            <a class="text-dark mb-2" href="checkout.html"><i class="fa fa-angle-right mr-2"></i>Checkout</a>
-            <a class="text-dark" href="contact.html"><i class="fa fa-angle-right mr-2"></i>Contact Us</a>
+            <a class="text-light mb-2" href="index.html"><i class="fa fa-angle-right mr-2"></i>Home</a>
+            <a class="text-light mb-2" href="shop.html"><i class="fa fa-angle-right mr-2"></i>Our Shop</a>
+            <a class="text-light mb-2" href="detail.html"><i class="fa fa-angle-right mr-2"></i>Shop Detail</a>
+            <a class="text-light mb-2" href="cart.html"><i class="fa fa-angle-right mr-2"></i>Shopping Cart</a>
+            <a class="text-light mb-2" href="checkout.html"><i class="fa fa-angle-right mr-2"></i>Checkout</a>
+            <a class="text-light" href="contact.html"><i class="fa fa-angle-right mr-2"></i>Contact Us</a>
           </div>
         </div>
         <div class="col-md-4 mb-5">
-          <h5 class="font-weight-bold text-dark mb-4">Quick Links</h5>
+          <h5 class="font-weight-bold text-light mb-4">Quick Links</h5>
           <div class="d-flex flex-column justify-content-start">
-            <a class="text-dark mb-2" href="index.html"><i class="fa fa-angle-right mr-2"></i>Home</a>
-            <a class="text-dark mb-2" href="shop.html"><i class="fa fa-angle-right mr-2"></i>Our Shop</a>
-            <a class="text-dark mb-2" href="detail.html"><i class="fa fa-angle-right mr-2"></i>Shop Detail</a>
-            <a class="text-dark mb-2" href="cart.html"><i class="fa fa-angle-right mr-2"></i>Shopping Cart</a>
-            <a class="text-dark mb-2" href="checkout.html"><i class="fa fa-angle-right mr-2"></i>Checkout</a>
-            <a class="text-dark" href="contact.html"><i class="fa fa-angle-right mr-2"></i>Contact Us</a>
+            <a class="text-light mb-2" href="index.html"><i class="fa fa-angle-right mr-2"></i>Home</a>
+            <a class="text-light mb-2" href="shop.html"><i class="fa fa-angle-right mr-2"></i>Our Shop</a>
+            <a class="text-light mb-2" href="detail.html"><i class="fa fa-angle-right mr-2"></i>Shop Detail</a>
+            <a class="text-light mb-2" href="cart.html"><i class="fa fa-angle-right mr-2"></i>Shopping Cart</a>
+            <a class="text-light mb-2" href="checkout.html"><i class="fa fa-angle-right mr-2"></i>Checkout</a>
+            <a class="text-light" href="contact.html"><i class="fa fa-angle-right mr-2"></i>Contact Us</a>
           </div>
         </div>
+        <!-- Newsletter and Social Media -->
         <div class="col-md-4 mb-5">
-          <h5 class="font-weight-bold text-dark mb-4">Newsletter</h5>
+          <h5 class="font-weight-bold text-light mb-4">Newsletter</h5>
           <form action="">
-            <!-- <div class="form-group">
-              <input type="text" class="form-control border-0 py-4" placeholder="Your Name" required="required" />
-            </div> -->
             <div class="form-group">
               <input type="email" class="form-control border-0 py-4" placeholder="Your Email" required="required" />
             </div>
@@ -61,7 +60,7 @@
               </button>
             </div>
           </form>
-          <h6 class="text-secondary text-dark mt-4 mb-3">Follow Us</h6>
+          <h6 class="text-secondary text-light mt-4 mb-3">Follow Us</h6>
           <div class="d-flex">
             <a class="btn btn-primary btn-square mr-2" href="#"><i class="fab fa-twitter"></i></a>
             <a class="btn btn-primary btn-square mr-2" href="#"><i class="fab fa-facebook-f"></i></a>
@@ -72,12 +71,13 @@
       </div>
     </div>
   </div>
+  <!-- Footer Bottom -->
   <div class="row border-top border-light mx-xl-5 py-4">
     <div class="col-md-6 px-xl-0">
-      <p class="mb-md-0 text-center text-md-left text-dark">
+      <p class="mb-md-0 text-center text-md-left text-light">
         &copy;
-        <a class="text-dark font-weight-semi-bold" href="#">Your Site Name</a>. All Rights Reserved. Designed by
-        <a class="text-dark font-weight-semi-bold" href="https://htmlcodex.com">HTML Codex</a><br />
+        <a class="text-light font-weight-semi-bold" href="#">Your Site Name</a>. All Rights Reserved. Designed by
+        <a class="text-light font-weight-semi-bold" href="https://htmlcodex.com">HTML Codex</a><br />
         Distributed By
         <a href="https://themewagon.com" target="_blank">ThemeWagon</a>
       </p>
@@ -87,5 +87,6 @@
     </div>
   </div>
 </div>
+
 <script src="js/cart.js"></script>
 <!-- Footer End -->
